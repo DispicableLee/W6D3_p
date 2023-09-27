d10a262975bcf6b5f8387075a07e389a88cd8222
DispicableLee <lee.robert053@gmail.com>
2023-09-27 14:30:53 -0400

    finished writing actions

--- practice/app/controllers/user_controller.rb
+++ practice/app/controllers/user_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
+    # GET
+    # show all users
     def index
         users = User.all
         render json: users
     end
-    def user_params #create or change
-        params.require(:user).permit(:body, :author_id)
+
+
+    # GET
+    # show a user by their id
+    def show
+        user = User.find(params[:id])
+        render json: user
     end
+
+
+
+
+    # POST
+    # create a new user
     def create
         user = User.new(user_params)
 
@@ -16,4 +29,48 @@ class UsersController < ApplicationController
             render user.errors.full_messages, status: 422
         end
     end
+
+
+    
+
+    # PATCH / PUT
+    # update a user's information
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render user.errors.full_message, status: 422
+        end
+    end
+
+
+    # DELETE
+    # deletes a specific user
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user && user.destroy
+            redirect_to user_url
+        else
+            render json: {'error': 'User does not exist'}
+        end
+    end
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+    def user_params #create or change
+        params.require(:user).permit(:body, :author_id)
+    end
+
 end
